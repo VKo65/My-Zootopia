2e526df5d06af641dd07ee6e3c671dcd49333a0d
VKo65 <volker@kolauch.de>
2024-12-10 14:43:55 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,12 +4,25 @@ def load_data(file_path):
         return json.load(file)
 
 animals = load_data("animals_data.json")
-for animal in range(len(animals)):
-    try:
-        print(f"\nName: {animals[animal]["name"]}")
-        print(f"Diet: {animals[animal]["characteristics"]["diet"]}")
-        print(f"Location: {animals[animal]["locations"][0]}")
-        print(f"Type: {animals[animal]["characteristics"]["type"]}")
-    except KeyError:
-        pass
 
+with open("animals_template.html", "r") as file:
+    html_content = file.read()
+
+
+def output_data():
+    output = ""
+    for animal in range(len(animals)):
+        try:
+            output += f"\nName: {animals[animal]["name"]}\n"
+            output += f"Diet: {animals[animal]["characteristics"]["diet"]}\n"
+            output += f"Location: {animals[animal]["locations"][0]}\n"
+            output += f"Type: {animals[animal]["characteristics"]["type"]}\n"
+        except KeyError:
+            pass
+    return output
+#print(output_data())
+html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", output_data())
+#print(html_content)
+
+with open("animal.html", "w") as new_file:
+    new_file.write(html_content)
